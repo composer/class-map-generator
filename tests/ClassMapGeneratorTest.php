c27da00dcd76423e8e3af7de600d74e04c9a38df
Jordi Boggiano <j.boggiano@seld.be>
2024-05-31 15:04:44 +0200

    Fix test

--- tests/ClassMapGeneratorTest.php
+++ tests/ClassMapGeneratorTest.php
@@ -244,13 +244,15 @@ class ClassMapGeneratorTest extends TestCase
     {
         $this->generator->scanPaths(__DIR__ . '/Fixtures/psrViolations', null, 'psr-4', 'ExpectedNamespace\\');
         $classMap = $this->generator->getClassMap();
+        $violations = $classMap->getPsrViolations();
+        sort($violations);
         self::assertSame(
             [
                 'Class ClassWithoutNameSpace located in ./tests/Fixtures/psrViolations/ClassWithoutNameSpace.php does not comply with psr-4 autoloading standard. Skipping.',
-                'Class UnexpectedNamespace\ClassWithNameSpaceOutsideConfiguredScope located in ./tests/Fixtures/psrViolations/ClassWithNameSpaceOutsideConfiguredScope.php does not comply with psr-4 autoloading standard. Skipping.',
                 'Class ExpectedNamespace\UnexpectedSubNamespace\ClassWithIncorrectSubNamespace located in ./tests/Fixtures/psrViolations/ClassWithIncorrectSubNamespace.php does not comply with psr-4 autoloading standard. Skipping.',
+                'Class UnexpectedNamespace\ClassWithNameSpaceOutsideConfiguredScope located in ./tests/Fixtures/psrViolations/ClassWithNameSpaceOutsideConfiguredScope.php does not comply with psr-4 autoloading standard. Skipping.',
             ],
-            $classMap->getPsrViolations()
+            $violations
         );
     }
 
